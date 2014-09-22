444d7e15cd1dc1374548b9e4bdc7539d278a6c5e
mstiles92 <mstiles92@gmail.com>
2014-09-22 01:51:41 -0400

    Let gradle set the version number in the reference class.

--- src/main/java/com/mstiles92/tcwarpoverlay/reference/Reference.java
+++ src/main/java/com/mstiles92/tcwarpoverlay/reference/Reference.java
@@ -25,7 +25,7 @@ package com.mstiles92.tcwarpoverlay.reference;
 public class Reference {
     public static final String MOD_ID = "TCWarpOverlay";
     public static final String MOD_NAME = "Thaumcraft Warp Overlay";
-    public static final String VERSION = "0.0.1-SNAPSHOT";
+    public static final String VERSION = "@VERSION@";
     public static final String DEPENDENCIES = "required-after:Thaumcraft@[4.2.0.0,)";
     public static final String CLIENT_PROXY_CLASS = "com.mstiles92.tcwarpoverlay.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.mstiles92.tcwarpoverlay.proxy.ServerProxy";
