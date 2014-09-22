dd4254c046d91a6fb5c8428b2b695933c1095664
mstiles92 <mstiles92@gmail.com>
2014-09-21 21:07:55 -0400

    Added main mod class and reference class.

---
+++ src/main/java/com/mstiles92/tcwarpoverlay/TCWarpOverlay.java
@@ -0,0 +1,23 @@
+package com.mstiles92.tcwarpoverlay;
+
+import com.mstiles92.tcwarpoverlay.reference.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class TCWarpOverlay {
+
+    @Mod.Instance(Reference.MOD_ID)
+    public static TCWarpOverlay instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/com/mstiles92/tcwarpoverlay/reference/Reference.java
@@ -0,0 +1,7 @@
+package com.mstiles92.tcwarpoverlay.reference;
+
+public class Reference {
+    public static final String MOD_ID = "TCWarpOverlay";
+    public static final String MOD_NAME = "Thaumcraft Warp Overlay";
+    public static final String VERSION = "0.0.1-SNAPSHOT";
+}
