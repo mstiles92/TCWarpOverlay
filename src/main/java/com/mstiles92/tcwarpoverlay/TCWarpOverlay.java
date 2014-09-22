f9e47b385b307dbe9d90dd1ddd4d48dc9cef7265
mstiles92 <mstiles92@gmail.com>
2014-09-21 21:12:49 -0400

    Add Thaumcraft 4.2.0.0 or higher as a required dependency.

--- src/main/java/com/mstiles92/tcwarpoverlay/TCWarpOverlay.java
+++ src/main/java/com/mstiles92/tcwarpoverlay/TCWarpOverlay.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES)
 public class TCWarpOverlay {
 
     @Mod.Instance(Reference.MOD_ID)
--- src/main/java/com/mstiles92/tcwarpoverlay/reference/Reference.java
+++ src/main/java/com/mstiles92/tcwarpoverlay/reference/Reference.java
@@ -4,4 +4,5 @@ public class Reference {
     public static final String MOD_ID = "TCWarpOverlay";
     public static final String MOD_NAME = "Thaumcraft Warp Overlay";
     public static final String VERSION = "0.0.1-SNAPSHOT";
+    public static final String DEPENDENCIES = "required-after:Thaumcraft@[4.2.0.0,)";
 }
